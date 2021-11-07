c668e4ee471da1029443aad1246d774be8d8964a
frank505 <akpufranklin2@gmail.com>
2021-11-07 23:16:04 +0100

    add gaurd

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { HashRouter , Route, Switch, Redirect, RouteComponentProps } from 'react-router-dom';
+import { Gaurd } from './gaurd/Gaurd';
+import './scss/style.scss';
+
+
+
+
+const loading = (
+  <div className="pt-3 text-center">
+    <div className="sk-spinner sk-spinner-pulse"></div>
+  </div>
+)
+
+// Containers
+const DefaultLayout:React.FunctionComponent<RouteComponentProps> = React.lazy(() => import('./layout/DefaultLayout'));
+
+// Pages
+const Login:React.FunctionComponent<RouteComponentProps>  = React.lazy(() => import('./views/login/Login'));
+const Register:React.FunctionComponent<RouteComponentProps> = React.lazy(() => import('./views/register/Register'));
+const Page404:React.FunctionComponent<RouteComponentProps>  = React.lazy(() => import('./views/pages/page404/Page404'));
+const Page500:React.FunctionComponent<RouteComponentProps>  = React.lazy(() => import('./views/pages/page500/Page500'));
+
+
+
+
+const App:React.FunctionComponent = () => {
+  return (
+    <HashRouter>
+    <React.Suspense fallback={loading}>
+      <Switch>
+        <Route exact={true} path="/login"  render={(props:any) => <Login {...props} />} />
+        <Route
+          exact={true}
+          path="/register"
+          render={(props) => <Register {...props} />}
+        />
+
+        <Route exact={true} path="/404"  render={(props:any) => <Page404 {...props} />} />
+        <Route exact={true}   path="/500"  render={(props:any) => <Page500 {...props} />} />
+        <Gaurd  path="/" name="Home" Token='medics-admin' routeRedirect="/login" Component={DefaultLayout} />
+      </Switch>
+    </React.Suspense>
+  </HashRouter>
+  )
+}
+
+export default App
+
+
+
+
---
+++ src/gaurd/Gaurd.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Redirect,RouteComponentProps } from 'react-router-dom';
+import Cookies from 'js-cookie';
+
+
+export interface IGaurd
+{
+Component:React.FunctionComponent<RouteComponentProps>
+Token:any,
+routeRedirect:string,
+name:string,
+path:string
+rest?:any,
+
+}
+
+export  const Gaurd: React.FC<IGaurd> = ({ Component,Token,routeRedirect,name,path, ...rest}) =>
+(
+    <Route {...rest} render={props => (
+        !Cookies.get(Token)
+            ? <Component {...props} />
+            : <Redirect to={{ pathname: routeRedirect, state: { from: props.location } }} />
+    )} />
+)
